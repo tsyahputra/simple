8d55c96e64067d2330a1e927eb267659102f86c0
tsyahputra <tsyahputra@mail.com>
2024-11-16 18:00:49 +0700

    First init

---
+++ lib/service/constants.dart
@@ -0,0 +1,19 @@
+class AppUrl {
+  static String apiUrl = 'http://127.0.0.1:8201';
+
+  static String masuk = '$apiUrl/masuk';
+  static String verifyCaptcha = '$apiUrl/verifycaptcha';
+
+  static String instances = '$apiUrl/instances';
+  static String viewInstance = '$instances/view';
+  static String addInstance = '$instances/add';
+  static String editInstance = '$instances/edit';
+
+  static String users = '$apiUrl/users';
+  static String beforeAddUser = '$users/beforeadd';
+  static String addUser = '$users/add';
+  static String viewUser = '$users/view';
+  static String editUser = '$users/edit';
+  static String editUserOnly = '$users/editonly';
+  static String changePassword = '$users/changepassword';
+}
